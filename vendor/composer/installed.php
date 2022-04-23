c30937aacc8c1f2e7112d98d6c37d3443b5037f8
trannhatthanh <20211tt1281@mail.tdc.edu.vn>
2022-04-23 09:42:49 +0700

    âs

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '69e1060fc794ad85e9f6e8fb7a5397d1cc03ec21',
+        'name' => 'thanhlk0803/mypackage',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tailieuweb/mypackage' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tailieuweb/mypackage',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '071250e444f58ee406ad5a1a815d5d33323c78ac',
+            'dev_requirement' => false,
+        ),
+        'thanhlk0803/mypackage' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '69e1060fc794ad85e9f6e8fb7a5397d1cc03ec21',
+            'dev_requirement' => false,
+        ),
+    ),
+);
